e3866c69b5d2a500b65c9dcd89ccfc9e16ff34ea
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-17 15:30:31 +0200

    useEffect basics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/1-useState/setup/4-useState-object";
+import Setup from "./tutorial/2-useEffect/setup/1-useEffect-basics";
 function App() {
   return (
     <div className="container">
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+  const resetValue = () => setValue(0);
+  const complexCounterIncrease = () => {
+    setTimeout(() => {
+      //setValue(value + 1);
+      setValue((prevState) => {
+        return prevState + 1;
+      });
+    }, 2000);
+  };
+  return (
+    <>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>regular counter</h2>
+        <h1>{value}</h1>
+        <button
+          type="button"
+          className="btn"
+          onClick={() => setValue(value + 1)}
+        >
+          Increase
+        </button>
+        <button
+          type="button"
+          className="btn"
+          onClick={() => setValue(value - 1)}
+        >
+          Decrease
+        </button>
+        <button type="button" className="btn" onClick={resetValue}>
+          Reset
+        </button>
+      </section>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>more complex counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={complexCounterIncrease}>
+          increase later
+        </button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
