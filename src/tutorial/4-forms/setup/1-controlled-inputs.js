55441e00de2ece13ef3d4c4ee4670f99da9932f0
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-18 12:40:39 +0200

    Controlled inputs with deletion from list button

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -8,9 +8,25 @@ import React, { useState } from "react";
 const ControlledInputs = () => {
   const [firstName, setfirstName] = useState("");
   const [email, setEmail] = useState("");
+  const [people, setPeople] = useState([]);
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(firstName, email);
+    if (firstName && email) {
+      const person = { id: new Date().getTime().toString(), firstName, email };
+      setPeople((oldPeople) => {
+        return [...people, person];
+      });
+      setfirstName("");
+      setEmail("");
+    } else {
+      alert("empty values...");
+    }
+  };
+  const handleClick = (id) => {
+    setPeople((people) => {
+      let newPeople = people.filter((person) => person.id !== id);
+      return newPeople;
+    });
   };
   return (
     <>
@@ -38,6 +54,22 @@ const ControlledInputs = () => {
           </div>
           <button type="submit">add person</button>
         </form>
+        {people.map((person) => {
+          const { id, firstName, email } = person;
+          return (
+            <div className="item" key={id}>
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+              <button
+                type="button"
+                className="btn"
+                onClick={() => handleClick(id)}
+              >
+                X
+              </button>
+            </div>
+          );
+        })}
       </article>
     </>
   );
