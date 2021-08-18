6caadd025982b6882d814372d9da80b76f51e972
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-18 11:25:00 +0200

    Short circuit evaluation

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
+  const firstValue = text || "hello world";
+  const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/*       <h1>{firstValue}</h1>
+      <h1>value : {secondValue}</h1> */}
+      <h1>{text || "prle"}</h1> // either/or displays which one the two are true
+      FIRST
+      <br></br>
+      {text && <h1>hello world</h1>} // if first condition(text) is
+      false/falsey, renders nothing. if first condition is true, renders 2nd
+      statement
+    </>
+  );
 };
 
 export default ShortCircuit;
