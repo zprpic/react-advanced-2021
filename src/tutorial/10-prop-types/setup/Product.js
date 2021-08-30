1cf48bc490a803fa2b2ea671f487be88ca7a8913
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-30 12:10:45 +0200

    Prop types and default props

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import defaultImage from "../../../assets/default-image.jpeg";
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({ image, name, price }) => {
+  return (
+    <article className="product">
+      <h4>{name}</h4>
+      <img src={image.url} alt={name} />
+      <p>${price}</p>
+    </article>
+  );
+};
+
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
+Product.defaultProps = {
+  name: "default name",
+  price: 3.99,
+  image: defaultImage,
 };
 
 export default Product;
